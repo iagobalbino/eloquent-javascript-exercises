03d052fe3adf58e848c11207c95f3e3ab8130cf8
Iago Balbino <iago_nobre@hotmail.com>
2021-07-27 14:43:01 -0300

    Added min function on chapter3

---
+++ chapter3/is-even-function.js
@@ -0,0 +1,16 @@
+// Is even function
+function isEven(n) {
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  } else if (n - 2) {
+    true
+  } else {
+    false
+  }
+}
+
+console.log(isEven(0));
+console.log(isEven(1));
+console.log(isEven(3));
---
+++ chapter3/min-function.js
@@ -0,0 +1,15 @@
+// Min function
+function min (n1, n2) {
+  minNumber = Math.min(n1, n2);
+  return console.log(minNumber);
+}
+
+// min(100, 30);
+
+// Arrow function
+min = (n1, n2) => {
+  minNumber = Math.min(n1, n2);
+  return console.log(minNumber);
+};
+
+min(10, 30);
