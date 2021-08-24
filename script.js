ce30e6b448f73f6134b4acdde19fee95da6527f1
Iago Balbino <iago_nobre@hotmail.com>
2021-08-24 11:43:41 -0300

    Working on function reverserArrayInPlace in script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // Rascunho
+
+function reverseArray(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    newArr.unshift(arr[i]);
+  }
+
+  return newArr;
+}
+
+const nome = ['I', 'A', 'G', 'O'];
+const num = [1, 2, 3, 4]
+
+// Parei tentando inverter as posições do array
+function reverseArrayInPlace(arr) {
+  for (let i = 0; i < (arr.length / 2); i++) {
+    let arrItem = arr[i];
+    console.log(arrItem);
+
+    if (arr.length - 1 - i === i) {
+      arr.unshift(arr[i]);
+      // console.log(arr);
+    }
+
+  }
+
+  // return console.log(arr);
+}
+
+console.log(reverseArrayInPlace(nome));
